35b0285538a9464a080dc6100d1af6521b12a9de
kratik8120 <115532945+kratik8120@users.noreply.github.com>
2023-09-10 16:01:42 +0530

    Update AdminCheck.java

--- app/src/main/java/com/mayank/industrial_training_app/AdminCheck.java
+++ app/src/main/java/com/mayank/industrial_training_app/AdminCheck.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AdminCheck extends AppCompatActivity {
+      ImageView img;
+    Button button1,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_check);
+        img=findViewById(R.id.imageView2);
+        button1=findViewById(R.id.button11);
+        button2=findViewById(R.id.button12);
     }
 }
