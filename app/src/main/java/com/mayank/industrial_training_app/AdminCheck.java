d0dd23fe536efea296564822b157a87251cb60e7
mayank <mayank.agrawal1604@gmail.com>
2023-09-10 14:46:28 +0530

    seventh Commit

---
+++ app/src/main/java/com/mayank/industrial_training_app/AdminCheck.java
@@ -0,0 +1,14 @@
+package com.mayank.industrial_training_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_check);
+    }
+}
---
+++ app/src/main/java/com/mayank/industrial_training_app/Splash_Screen.java
@@ -0,0 +1,14 @@
+package com.mayank.industrial_training_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
