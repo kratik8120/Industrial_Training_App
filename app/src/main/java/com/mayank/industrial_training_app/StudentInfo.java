ea7453ca456d3f1f70edcb25dfab4531d84edc46
mayank <mayank.agrawal1604@gmail.com>
2023-09-12 21:23:36 +0530

    eighth Commit

---
+++ app/src/main/java/com/mayank/industrial_training_app/StudentInfo.java
@@ -0,0 +1,14 @@
+package com.mayank.industrial_training_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info);
+    }
+}
