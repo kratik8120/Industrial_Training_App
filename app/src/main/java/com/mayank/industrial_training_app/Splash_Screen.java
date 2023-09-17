155076b36c866bb48257eac14ab8717a7716bcdd
kratik8120 <115532945+kratik8120@users.noreply.github.com>
2023-09-17 22:12:09 +0530

    Update Splash_Screen.java

--- app/src/main/java/com/mayank/industrial_training_app/Splash_Screen.java
+++ app/src/main/java/com/mayank/industrial_training_app/Splash_Screen.java
@@ -6,9 +6,33 @@ import android.os.Bundle;
 
 public class Splash_Screen extends AppCompatActivity {
 
+     TextView textView,base;
+    Animation bottom,top;
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+         top=AnimationUtils.loadAnimation(this,R.anim.top);
+        bottom=AnimationUtils.loadAnimation(this,R.anim.bottom);
+        textView=findViewById(R.id.textView5);
+        image=findViewById(R.id.imageView3);
+        base=findViewById(R.id.base);
+
+        image.setAnimation(top);
+        textView.setAnimation(bottom);
+        base.setAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
     }
+    
 }
